95b24a9f2585bebaa3068c5edd30a37148446648
shahrahim <64368241+shahrahim@users.noreply.github.com>
2023-01-03 19:30:09 -0500

    Added robust fix for comparing prayerTimer date ranges (#9)
    * Added robust fix for comparing prayerTimer date ranges
    
    * removing unnecessry spaces
    
    * Using native CharacterSet

--- Tests/PrayKitTests/Models/PrayerTimerTests.swift
+++ Tests/PrayKitTests/Models/PrayerTimerTests.swift
@@ -605,7 +605,8 @@ extension PrayerTimerTests {
 private extension PrayerTimerTests {
     func assertCalculations(for prayerTimer: PrayerTimer, minutes: Int, lastAdhanTime: Date? = nil) {
         XCTAssertEqual(prayerTimer.timeRemaining, Double(minutes) * 60)
-        XCTAssertEqual(prayerTimer.timeRange.formatted(Date.ComponentsFormatStyle(style: .condensedAbbreviated, fields: [.minute])), "\(abs(minutes))min")
+        XCTAssertEqual(prayerTimer.timeRange.formatted(Date.ComponentsFormatStyle(style: .condensedAbbreviated, fields: [.minute]))
+            .components(separatedBy: .decimalDigits.inverted).joined(), "\(abs(minutes))")
         switch prayerTimer.timerType {
         case .stopwatch:
             XCTAssertEqual(prayerTimer.progressRemaining, 1 + prayerTimer.timeRemaining / prayerTimer.timeDuration, accuracy: 0.0001)
